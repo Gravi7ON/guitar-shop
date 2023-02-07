7eda4774cfac7b6e83f7ccce1510386930ab374b
Dmitry Radionov <radionovdmr@gmail.com>
2023-02-07 15:20:53 +0500

    Добавляет конфигурации для mongodb, генерирует модули auth, user

--- backend/apps/users/src/app/app.module.ts
+++ backend/apps/users/src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
+import { validateEnvironments } from './env.validation';
+import { getMongoDbConfig } from '../config/mongodb.config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+      validate: validateEnvironments,
+    }),
+    MongooseModule.forRootAsync(getMongoDbConfig()),
+    AuthModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
